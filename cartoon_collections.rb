29d9df4c7e3fd63f9c5663253be0c7eb3c652195
Irina Bright <missirinabright@gmail.com>
2019-05-29 22:50:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? {|call| call.length > 4}
 end
 
-snacks = ["crackers", "gouda", "thyme"]
+cheese = ["crackers", "gouda", "thyme"]
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
